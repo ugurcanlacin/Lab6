457b0a2e8b14c711d64e1a45422001be51eb1ebb
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-10-05 13:51:18 +0200

    initial commit

---
+++ R/knapsack brute force.R
@@ -0,0 +1,49 @@
+set.seed(42)
+n <- 16
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+
+
+
+brute_force_knapsack <- function(x,W){
+  
+  listOfCombinations <- list()
+  for(i in 1:nrow(x)){
+    listOfCombinations[[i]] <- combn(rownames(x), i, paste, collapse = " ")
+  }
+  
+  listOfWeights <- list()
+  for(i in 1:nrow(x)){
+    listOfWeights[[i]] <- combn(x$w, i,sum)
+  }
+  
+  listOfValues <- list()
+  for(i in 1:nrow(x)){
+    listOfValues[[i]] <- combn(x$v, i,sum)
+  }
+
+  vectorOfCombinations <- unlist(listOfCombinations)
+  vectorOfWeights <- unlist(listOfWeights)
+  vectorOfValues <- round(unlist(listOfValues),0)
+  
+  weightsIndexesUnderTheCapacity <- which(vectorOfWeights < W)
+  validValues <- vectorOfValues[weightsIndexesUnderTheCapacity]
+  maximumValidValue <- max(validValues)
+  
+  validValuesIndexesInCombinationsVector <- which(vectorOfValues == maximumValidValue)
+  validCombination <- vectorOfCombinations[validValuesIndexesInCombinationsVector]
+  
+  bestCombinationList <- list(value = maximumValidValue, elements = validCombination)
+  return(bestCombinationList)
+}
+
+# Start the clock!
+ptm <- proc.time()
+brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+# Sys.sleep(1)
+# Stop the clock
+proc.time() - ptm
+
